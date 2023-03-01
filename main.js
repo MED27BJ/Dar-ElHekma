490509c25093494b1db518d8c15ae0c2f31b0956
sa00ka <krouf.education@gmail.com>
2023-03-01 22:49:48 +0100

    card

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var biblio=[
+const biblio=[
     { 
         theme:"Literature",
         image:"./images/Taha Houssein/El Ayem.jfif", 
         titre:"El Ayem",
         auteur:"Taha Houssein",
-         },
+         }
 ]
