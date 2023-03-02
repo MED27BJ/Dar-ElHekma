6e007d1eca153dd1e057d6c3eceed61242e17454
BEJI <119804271+MED27BJ@users.noreply.github.com>
2023-03-02 04:11:40 +0100

    zid chouf hani zedek chouaya

--- main.js
+++ main.js
@@ -1,8 +1,81 @@
-var biblio=[
-    { 
-        theme:"Literature",
-        image:"./images/Taha Houssein/El Ayem.jfif", 
-        titre:"El Ayem",
-        auteur:"Taha Houssein",
-         },
-]
+var biblio = [
+    {
+      theme: "Literature",
+      image: "./images/Taha Houssein/El Ayem.jfif",
+      titre: "El Ayem",
+      auteur: "Taha Houssein",
+    },
+    {
+      theme: "Literature",
+      image: "./images/Taha Houssein/Elwaed Elhak.jfif",
+      titre: "Elwaed Elhak",
+      auteur: "Taha Houssein",
+    },
+    {
+      theme: "Literature",
+      image: "./images/Taha Houssein/Chaikhan.jfif",
+      titre: "AL Aaamal Alkamila",
+      auteur: "Taha Houssein",
+    },
+    {
+      theme: "Literature",
+      image: "./images/Mahmoud Messadi/Essod.jfif",
+      titre: "Essod",
+      auteur: "Mahmoud Messadi",
+    },
+    {
+      theme: "Literature",
+      image: "./images/Mahmoud Messadi/AL Aaamal Alkamila.jfif",
+      titre: "AL Aaamal Alkamila",
+      auteur: "Mahmoud Messadi",
+    },
+    {
+      theme: "Literature",
+      image: "./images/Mahmoud Messadi/Haddatha Abouhouraira.jfif",
+      titre: "Haddatha Abouhouraira",
+      auteur: "Mahmoud Messadi",
+    },
+    {
+      theme: "Enfants",
+      image: "./images/Enfants/Aboukharbouch.jfif",
+      titre: "Aboukharbouch",
+      auteur: "Kamel Kilani",
+    },
+    {
+      theme: "Enfants",
+      image: "./images/Kamel Kilani/Le chasseur et le lapin.jfif",
+      titre: "Le chasseur et le lapin",
+      auteur: "Kamel Kilani",
+    },
+    {
+      theme: "Enfants",
+      image: "./images/Bidaya/La robe verte.jfif",
+      titre: "La robe verte",
+      auteur: "Bidaya",
+    },
+    {
+      theme: "Enfants",
+      image: "./images/Bidaya/Blanche Neige.jfif",
+      titre: "Banche Neige",
+      auteur: "Bidaya",
+    },
+  
+    {
+      theme: "Poesie",
+      image: "./images/Nizar Kabani/Alaamal Acherya Alkamila.jfif",
+      titre: "Alaamal Acherya Alkamila",
+      auteur: "Nizar Kabani",
+    },
+    {
+      theme: "Poesie",
+      image: "./images/Abou Alkacem Achabi/Adiwane.jfif",
+      titre: "Adiwane",
+      auteur: "Abou Alkacem Achabi",
+    },
+    {
+      theme: "Poesie",
+      image: "./images/Ahmed Chaouki/Achaoukiat.jfif",
+      titre: "Achaoukiat",
+      auteur: "Ahmed Chaouki",
+    },
+  ];
